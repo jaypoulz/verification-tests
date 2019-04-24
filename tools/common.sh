1ccc0e9645bf9cec57d60adcfc72591ef4d6aff0
Jeremy Poulin <jpoulin@redhat.com>
2019-04-24 10:26:44 -0400

    Upgraded ruby version to a branch that is maintained and available in SCL for x86_64

--- tools/common.sh
+++ tools/common.sh
@@ -20,8 +20,8 @@ function install_rvm_if_ruby_is_outdated()
         gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
         curl -sSL https://get.rvm.io | bash -s stable
         source ~/.rvm/scripts/rvm
-        rvm install 2.3.3
-        rvm --default use 2.3.3
+        rvm install 2.4
+        rvm --default use 2.4
     fi
 }
 
